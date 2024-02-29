fcc80e70412659b3fcec33f94deea6b7595abc20
adrisali27 <adrisali@ucm.es>
2024-02-29 15:25:44 +0100

    enemigo photon Destructor

---
+++ src/photonDestructor.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser'
+
+
+
+
+export default class photonDestructor extends Phaser.GameObjects.Sprite {
+
+    /**
+     * Constructor del enemigo
+     * @param {Phaser.Scene} scene Escena a la que pertenece el jugador
+     * @param {number} x Coordenada X
+     * @param {number} y Coordenada Y
+     */
+    constructor(scene, x, y) {
+        super(scene, x, y, 'photonDestructor');
+        this.setScale(3,3);
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        // Queremos que el jugador no se salga de los límites del mundo
+        this.body.setCollideWorldBounds();
+    }
+}
