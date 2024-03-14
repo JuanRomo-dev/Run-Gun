9d8b7ab7698d95c1521b38fbe7a3d61e04ff48eb
adrisali27 <adrisali@ucm.es>
2024-03-14 15:41:33 +0100

    principios del movimiento

--- src/photonDestructor.js
+++ src/photonDestructor.js
@@ -6,6 +6,7 @@ export default class PhotonDestructor extends Phaser.GameObjects.Sprite {
      * @param {Phaser.Scene} scene Escena a la que pertenece el jugador
      * @param {number} x Coordenada X
      * @param {number} y Coordenada Y
+     * @param {Phaser.GameObjects.Sprite} player jugador
      */
     constructor(scene, player, x, y) {
         super(scene, x, y, "photonDestructor");
@@ -15,5 +16,27 @@ export default class PhotonDestructor extends Phaser.GameObjects.Sprite {
         this.scene.physics.add.existing(this);
         this.scene.physics.add.collider(this, player);
         this.body.setCollideWorldBounds();
+        this.speed = 100;
+        this.jumpSpeed = -100;
+        //this.body.setVelocityX(this.speed);
     }
+
+/**
+   * Métodos preUpdate de Phaser. En este caso solo se encarga del movimiento del enemigo.
+   * @override
+   */
+    preUpdate(t, dt) {
+        super.preUpdate(t, dt);
+        //this.body.setVelocityX(-this.speed);
+        /*
+        if (this.player.x < this.x) { falla aqui
+            this.body.setVelocityX(-this.speed);
+        }else if (this.player.x > this.x){
+            this.body.setVelocityX(this.speed);
+        }else{
+            this.body.setVelocityX(0);
+        }
+        */
+    }
+
 }
