1518818bd281b7249d5a2e06b04f3db431a123e4
Adrisali <adrisali@ucm.es>
2024-05-09 17:15:51 +0200

    efecto rebote en consumibles y armas

--- src/scenes/level.js
+++ src/scenes/level.js
@@ -259,8 +259,7 @@ export default class Level extends Phaser.Scene {
     if (enemy.life <= 0) {
       this.player.updateScore(enemy.score)
       enemy.dead(this)
-      let random = Math.floor(Math.random() * 10) + 1;
-      console.log(random);
+      let random = Math.floor(Math.random() * 10) + 1; //Probabilidad de aparecer consumible
       if(random === 1){
         this.consumibles.push(new Heart(this, enemy.x, enemy.y));
         this.consumibleGroup = new ConsumiblesGroup(this, this.consumibles, this.player)
